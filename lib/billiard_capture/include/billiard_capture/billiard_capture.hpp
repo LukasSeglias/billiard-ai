ffa13190c329753ec3040e53c7b503338ef546a8
Lukas Seglias <lukas.seglias@gmail.com>
2021-04-03 09:44:31 +0200

    Integrated Intel RealSense SDK and wrote wrapper for it

--- lib/billiard_capture/include/billiard_capture/billiard_capture.hpp
+++ lib/billiard_capture/include/billiard_capture/billiard_capture.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <opencv2/opencv.hpp>
+#include <utility>
 #include "macro_definition.hpp"
 
 namespace billiard::capture {
@@ -22,6 +23,31 @@ namespace billiard::capture {
         Device* device;
         Stream* stream;
         ImageStream* imageStream;
+    };
+
+    struct EXPORT_BILLIARD_CAPTURE_LIB CameraFrames {
+        cv::Mat color;
+        cv::Mat depth;
+        cv::Mat colorizedDepth;
+        CameraFrames(const cv::Mat& color, const cv::Mat& depth, const cv::Mat& colorizedDepth): color(color), depth(depth), colorizedDepth(colorizedDepth) {}
+        CameraFrames(const CameraFrames& other) = default;
+        CameraFrames(CameraFrames&& other) = default;
+        ~CameraFrames() = default;
+    };
+
+    class Camera;
+
+    class EXPORT_BILLIARD_CAPTURE_LIB CameraCapture {
+    public:
+        CameraCapture() = default;
+        CameraCapture(const CameraCapture& other) = delete;
+        CameraCapture(CameraCapture&& other) = delete;
+        bool open();
+        void close();
+        [[nodiscard]] CameraFrames read() const;
+        ~CameraCapture();
+    private:
+        Camera* camera;
     };
 
 }
--- lib/billiard_capture/src/billiard_capture.cpp
+++ lib/billiard_capture/src/billiard_capture.cpp
@@ -12,9 +12,13 @@
 #include <ebus/PvStreamU3V.h>
 #include <ebus/PvBuffer.h>
 
+#include <librealsense2/rs.hpp>
+#include <librealsense2/hpp/rs_internal.hpp>
+
 #include <list>
 
 #include "image_stream.hpp"
+#include "camera.hpp"
 
 // @Source: This code is heavily based on the eBUS SDK Example 'PVStreamSample'
 
@@ -120,58 +124,111 @@ namespace billiard::capture {
         }
     }
 
-}
+    ImageCapture::~ImageCapture() {
+        // Delete in reverse order so that everything is shut down nicely
+        delete imageStream;
+        delete stream;
+        delete device;
+    }
 
-billiard::capture::ImageCapture::~ImageCapture() {
-    // Delete in reverse order so that everything is shut down nicely
-    delete imageStream;
-    delete stream;
-    delete device;
-}
+    bool ImageCapture::open(std::string macAddress) {
 
-bool billiard::capture::ImageCapture::open(std::string macAddress) {
+        PvString connectionId;
+        if (findGigEDeviceWithMAC(macAddress, &connectionId)) {
 
-    PvString connectionId;
-    if (findGigEDeviceWithMAC(macAddress, &connectionId)) {
+            device = ConnectToDevice(connectionId);
+            if (device != nullptr) {
 
-        device = ConnectToDevice(connectionId);
-        if (device != nullptr) {
+                stream = OpenStream(connectionId);
+                if (stream != nullptr) {
 
-            stream = OpenStream(connectionId);
-            if (stream != nullptr) {
+                    ConfigureStream(device->device, stream->stream);
+                    CreateStreamBuffers(device->device, stream->stream, stream->buffers);
 
-                ConfigureStream(device->device, stream->stream);
-                CreateStreamBuffers(device->device, stream->stream, stream->buffers);
+                    imageStream = new ImageStream(*device, *stream);
+                    imageStream->start();
 
-                imageStream = new ImageStream(*device, *stream);
-                imageStream->start();
+                    std::cout << "open succeeded" << std::endl;
 
-                std::cout << "open succeeded" << std::endl;
-
-                return true;
+                    return true;
+                }
             }
         }
+
+        return false;
     }
 
-    return false;
-}
+    cv::Mat ImageCapture::read() const {
+        if (imageStream != nullptr) {
+            cv::Mat image;
+            if (imageStream->read(image)) {
+                return image;
+            }
+        }
+        return cv::Mat();
+    }
 
-cv::Mat billiard::capture::ImageCapture::read() const {
-    if (imageStream != nullptr) {
-        cv::Mat image;
-        if (imageStream->read(image)) {
-            return image;
+    void ImageCapture::close() {
+        // Delete in reverse order so that everything is shut down nicely
+        delete imageStream;
+        imageStream = nullptr;
+        delete stream;
+        stream = nullptr;
+        delete device;
+        device = nullptr;
+    }
+
+    bool CameraCapture::open() {
+        rs2::context ctx;
+        std::cout << "librealsense " << RS2_API_VERSION_STR << std::endl;
+        std::cout << "RealSense devices connected: " << ctx.query_devices().size() << std::endl;
+
+        rs2::config cfg;
+        cfg.enable_stream(RS2_STREAM_COLOR, 1920, 1080, RS2_FORMAT_BGR8, 30);
+        cfg.enable_stream(RS2_STREAM_DEPTH, 848, 480, RS2_FORMAT_Z16, 30);
+
+        camera = new Camera();
+        camera->pipe.start(cfg);
+
+        return true;
+    }
+
+    void CameraCapture::close() {
+        if (camera != nullptr) {
+            camera->pipe.stop();
         }
     }
-    return cv::Mat();
-}
 
-void billiard::capture::ImageCapture::close() {
-    // Delete in reverse order so that everything is shut down nicely
-    delete imageStream;
-    imageStream = nullptr;
-    delete stream;
-    stream = nullptr;
-    delete device;
-    device = nullptr;
+    cv::Mat toMat(const rs2::frame& frame, int type) {
+        if (!frame || frame.get_data() == nullptr) {
+            return cv::Mat {};
+        }
+        const int width = frame.as<rs2::video_frame>().get_width();
+        const int height = frame.as<rs2::video_frame>().get_height();
+        cv::Mat image(cv::Size(width, height), type, (void*)frame.get_data(), cv::Mat::AUTO_STEP);
+        // Clone in order to copy image content so that original frame can be freed
+        return image.clone();
+    }
+
+    CameraFrames CameraCapture::read() const {
+
+        rs2::frameset data = camera->pipe.wait_for_frames();
+
+        rs2::depth_frame depthFrame = data.get_depth_frame();
+        cv::Mat depth = toMat(depthFrame, CV_8UC1);
+
+        cv::Mat colorizedDepth;
+        if (depthFrame && depthFrame.get_data() != nullptr) {
+            rs2::colorizer color_map;
+            colorizedDepth = toMat(depthFrame.apply_filter(color_map),CV_8UC3);
+        }
+
+        cv::Mat color = toMat(data.get_color_frame(), CV_8UC3);
+
+        return CameraFrames(color, depth, colorizedDepth);
+    }
+
+    CameraCapture::~CameraCapture() {
+        delete camera;
+    }
 }
