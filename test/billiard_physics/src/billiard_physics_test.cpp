7800ecaf53a4ce989608713e53f59507a5addf54
LucaRitz <ritz.luca@gmail.com>
2021-10-14 10:00:22 +0200

    T-6: Improved ball collision detection

--- test/billiard_physics/src/billiard_physics_test.cpp
+++ test/billiard_physics/src/billiard_physics_test.cpp
@@ -44,6 +44,47 @@ TEST(solveQuarticFormula, shouldGetCorrectResult2) {
     assertComplex(expected, result);
 }
 
+TEST(solveQuarticFormula, shouldGetCorrectResult3) {
+    std::vector<std::complex<double>> expected{
+            0.11314,
+            { 0.715435, 0.839049 },
+            { 0.715435, -0.839049 },
+            { 1.31773 }
+    };
+
+    double a = 0.009604;
+    double b = -0.0274841;
+    double c = 0.0327719;
+    double d = -0.018757;
+    double e = 0.00174089;
+
+    // Act
+    auto result = billiard::physics::intersection::solveQuarticFormula(a, b, c, d, e);
+
+    // Assert
+    assertComplex(expected, result);
+}
+
+TEST(timeToCollision, shouldGetCorrectResult) {
+    glm::vec2 acceleration1 {94.7511, -171.576};
+    glm::vec2 acceleration2 {-0, -0};
+    glm::vec2 velocity1 {-67.7881, 122.751};
+    glm::vec2 velocity2 {0, 0};
+    glm::vec2 position1 {-868.681, 365.701};
+    glm::vec2 position2 {-899.492, 425.088};
+    float radius = 26.15f;
+    float diameter = radius * 2;
+
+    // Act
+    auto result = billiard::physics::timeToCollision(acceleration1, velocity1, position1,
+                                                     acceleration2, velocity2, position2,
+                                                     diameter, radius);
+
+    // Assert
+    ASSERT_TRUE(result);
+    ASSERT_NEAR(0.11314, *result, 0.0001f);
+}
+
 void assertComplex(const std::vector<std::complex<double>>& expected, const std::vector<std::complex<double>>& actual) {
     ASSERT_EQ(expected.size(), actual.size());
     for (int i = 0; i < actual.size(); i++) {
