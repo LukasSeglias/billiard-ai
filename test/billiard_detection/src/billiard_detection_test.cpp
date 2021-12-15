aea78e9bf57be2ce5165a5a00d68eedb49b060cb
Lukas Seglias <lukas.seglias@gmail.com>
2021-12-15 10:58:53 +0100

    Doku Resultate Detektion

--- test/billiard_detection/src/billiard_detection_test.cpp
+++ test/billiard_detection/src/billiard_detection_test.cpp
@@ -120,6 +120,9 @@ TEST(BallDetectionTests, detection_accuracy) {
 
 TEST(BallDetectionTests, stats) {
 
+    bool writeBadDetectionImages = false;
+    float criticalModelDistance = 7; // In millimeters
+
     std::vector<std::string> testcasesPaths = {
             "./resources/test_detection/with_projector_on/without_halo/testcases.json",
             "./resources/test_detection/with_projector_on/with_halo/testcases.json",
@@ -137,6 +140,8 @@ TEST(BallDetectionTests, stats) {
     }
 
     std::cout << "Got " << testcases.cases.size() << " images" << std::endl;
+
+    int badDetectionNumber = 1;
 
     int totalGhosts = 0;
     int totalLost = 0;
@@ -181,6 +186,33 @@ TEST(BallDetectionTests, stats) {
             for (auto& distance : pixelStats.distances) {
                 allPixelDistances.push_back(distance);
             }
+
+            for (int i = 0; i < expectedModelState._balls.size(); i++) {
+                auto& modelDistance = modelStats.distances[i];
+                auto& pixelDistance = pixelStats.distances[i];
+                auto actualPixelBall = pixelState._balls[pixelStats.matchedActualBalls[i]];
+
+                if (modelDistance > criticalModelDistance) {
+                    auto& pixelBall = expectedPixelState._balls[i];
+
+                    double radius = detectionConfig->ballRadiusInPixel;
+                    const cv::Rect& roi = cv::Rect{
+                            (int) ((double) pixelBall._position.x - radius),
+                            (int) ((double) pixelBall._position.y - radius),
+                            (int) (2 * radius),
+                            (int) (2 * radius)
+                    };
+                    cv::Point2i roiCenterPoint { (int)radius, (int)radius };
+                    cv::Point2i expectedPixelPoint { (int)pixelBall._position.x, (int)pixelBall._position.y };
+                    cv::Point2i actualPixelPoint { (int)actualPixelBall._position.x, (int)actualPixelBall._position.y };
+
+                    cv::Mat ballImage = frame(roi);
+                    cv::circle(ballImage, roiCenterPoint, 1, cv::Scalar(255, 255, 0), 1, cv::LINE_AA);
+                    cv::circle(ballImage, actualPixelPoint - expectedPixelPoint + roiCenterPoint, 1, cv::Scalar(0, 255, 255), 1, cv::LINE_AA);
+
+                    if (writeBadDetectionImages) cv::imwrite("bad_detection_" + std::to_string(badDetectionNumber++) + "_" + std::to_string(modelDistance) + "_" + std::to_string(pixelDistance) + ".png", ballImage);
+                }
+            }
         }
 
         totalLost += modelStats.lostBalls.size();
@@ -202,5 +234,130 @@ TEST(BallDetectionTests, stats) {
     FiveNumberSummary modelSummary = calculateFiveNumberSummary(allModelDistances);
     std::cout << "Model distance: count=" << allModelDistances.size() << ", " << modelSummary << std::endl;
 
+}
+
+
+TEST(BallDetectionTests, stats_per_type) {
+
+    std::vector<std::string> testcasesPaths = {
+            "./resources/test_detection/with_projector_on/with_halo_2/testcases.json"
+    };
+
+    std::vector<std::string> labels = {
+            "BROWN",
+            "PINK",
+            "RED",
+            "BLACK",
+            "YELLOW",
+            "WHITE",
+            "BLUE",
+            "GREEN",
+            "UNKNOWN"
+    };
+
+    cv::Size imageSize = getImageSize();
+    billiard::detection::Table table = getTable();
+    billiard::detection::ArucoMarkers markers = getMarkers();
+    billiard::detection::CameraIntrinsics intrinsics = getIntrinsics_realsense_hd();
+
+    DetectionTestCases testcases;
+    for (auto& testcasePath : testcasesPaths) {
+        testcases = merge(testcases, loadDetectionTestCases(testcasePath));
+    }
+
+    std::cout << "Got " << testcases.cases.size() << " images" << std::endl;
+
+    std::unordered_map<std::string, FiveNumberSummary> pixelSummaryPerLabel;
+    std::unordered_map<std::string, FiveNumberSummary> modelSummaryPerLabel;
+    std::unordered_map<std::string, int> countPerLabel;
+
+    for (auto& label : labels) {
+
+        std::vector<float> allPixelDistances;
+        std::vector<float> allModelDistances;
+
+        for (int testcaseIndex = 0; testcaseIndex < testcases.cases.size(); testcaseIndex++) {
+
+            DetectionTestCase testcase = testcases.cases[testcaseIndex];
+            std::string imagePath = testcase.image;
+            cv::Mat frame = cv::imread(imagePath, cv::IMREAD_COLOR);
+            cv::resize(frame, frame, imageSize);
+
+            auto detectionConfig = std::make_shared<billiard::detection::DetectionConfig>(billiard::detection::configure(frame, table, markers, intrinsics));
+            if (!detectionConfig->valid) {
+                std::cout << "Unable to configure detection" << std::endl;
+                return;
+            }
+
+            if (!billiard::snooker::configure(*detectionConfig)) {
+                std::cout << "Unable to configure snooker detection" << std::endl;
+                return;
+            }
+
+            billiard::detection::State pixelState = billiard::snooker::detect(billiard::detection::State(), frame);
+            billiard::detection::State modelState = billiard::detection::pixelToModelCoordinates(*detectionConfig, pixelState);
+
+            billiard::detection::State expectedPixelState = toPixelState(testcase.balls);
+            billiard::detection::State expectedModelState = toState(testcase.balls);
+
+            DetectionStats modelStats = compare(testcase, expectedModelState, modelState);
+            for (int i = 0; i < expectedModelState._balls.size(); i++) {
+                if (expectedModelState._balls[i]._type == label) {
+                    auto& distance = modelStats.distances[i];
+                    allModelDistances.push_back(distance);
+                }
+            }
+
+            if (!expectedPixelState._balls.empty()) {
+                DetectionStats pixelStats = compare(testcase, expectedPixelState, pixelState);
+
+                for (int i = 0; i < expectedModelState._balls.size(); i++) {
+                    if (expectedModelState._balls[i]._type == label) {
+                        auto& distance = pixelStats.distances[i];
+                        allPixelDistances.push_back(distance);
+                    }
+                }
+            }
+        }
+
+        std::cout << "Label: " << label << std::endl;
+        if (!allPixelDistances.empty()) {
+            std::sort(allPixelDistances.begin(), allPixelDistances.end());
+            FiveNumberSummary pixelSummary = calculateFiveNumberSummary(allPixelDistances);
+            std::cout << "Pixel distance: count=" << allPixelDistances.size() << ", " << pixelSummary << std::endl;
+            pixelSummaryPerLabel[label] = pixelSummary;
+        }
+        if (!allModelDistances.empty()) {
+            std::sort(allModelDistances.begin(), allModelDistances.end());
+            FiveNumberSummary modelSummary = calculateFiveNumberSummary(allModelDistances);
+            std::cout << "Model distance: count=" << allModelDistances.size() << ", " << modelSummary << std::endl;
+            modelSummaryPerLabel[label] = modelSummary;
+            countPerLabel[label] = allModelDistances.size();
+        }
+    }
+
+    std::cout << "Kugelfarbe & Anzahl Kugeln & Minimum & Unteres Quartil & Median & Oberes Quartil & Maximum" << "\\\\" << std::endl;
+    for (auto& label : labels) {
+
+        if (modelSummaryPerLabel.count(label) == 0) continue;
+        FiveNumberSummary summary = modelSummaryPerLabel[label];
+        int count = countPerLabel[label];
+
+        std::cout << label
+                << " " << "&" << " "
+                << std::to_string(count) << ""
+                << " " << "&" << " "
+                << std::to_string(summary.min) << "mm"
+                << " " << "&" << " "
+                << std::to_string(summary.lowerQuartile) << "mm"
+                << " " << "&" << " "
+                << std::to_string(summary.median) << "mm"
+                << " " << "&" << " "
+                << std::to_string(summary.upperQuartile) << "mm"
+                << " " << "&" << " "
+                << std::to_string(summary.max) << "mm"
+                << " " << "\\\\" << " "
+                << std::endl;
+    }
 }
 
